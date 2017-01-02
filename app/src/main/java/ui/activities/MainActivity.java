d2fa489aacc3753ce454455162217211daf2805f
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-03 01:21:00 +0700

    Reset Activity + Add Data Binding

--- app/src/main/java/ui/activities/MainActivity.java
+++ app/src/main/java/ui/activities/MainActivity.java
@@ -1,16 +1,16 @@
-package io.github.reiiyuki.findmything;
+package ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity  {
+import io.github.reiiyuki.findmything.R;
 
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
 }
