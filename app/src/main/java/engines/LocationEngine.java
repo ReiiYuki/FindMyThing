8d7d7c55a9406eff93deb5ebee1a236c56f0d89b
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-03 04:09:39 +0700

    Init Fragment

--- app/src/main/java/engines/LocationEngine.java
+++ app/src/main/java/engines/LocationEngine.java
@@ -1,5 +1,8 @@
 package engines;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
 import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
@@ -13,7 +16,12 @@ public class LocationEngine {
     private LocationEngine locationEngine;
 
     private LocationEngine(){
-
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+//                    REQUEST_CODE_ASK_PERMISSIONS);
+//            return;
+//        }
     }
 
     public LocationEngine getInstance(){
--- app/src/main/java/ui/activities/MainActivity.java
+++ app/src/main/java/ui/activities/MainActivity.java
@@ -1,21 +1,51 @@
 package ui.activities;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import io.github.reiiyuki.findmything.R;
 import io.github.reiiyuki.findmything.databinding.ActivityMainBinding;
+import ui.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity  {
 
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
+    private Fragment homeFragment;
+    private Intent intent;
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.testText.setText("Test Data Binding");
+        initDataBinding();
+        initIntent();
+        initFragment();
+        initTransaction();
+        beginHome();
     }
 
+    private void initDataBinding(){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    private void initIntent(){
+        intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
+    }
+
+    private void initFragment(){
+        homeFragment = new HomeFragment();
+    }
+
+    private void initTransaction(){
+        transaction = getSupportFragmentManager().beginTransaction();
+    }
+
+    private void beginHome(){
+        transaction.replace(R.id.fragment_frame,homeFragment);
+        transaction.commit();
+    }
 }
