8723dbae2827370cc4ae3179d7d6d870a0e22e5d
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-03 01:26:20 +0700

    Data Binding Success

--- app/src/main/java/ui/activities/MainActivity.java
+++ app/src/main/java/ui/activities/MainActivity.java
@@ -1,16 +1,21 @@
 package ui.activities;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import io.github.reiiyuki.findmything.R;
+import io.github.reiiyuki.findmything.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity  {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.testText.setText("Test Data Binding");
     }
 
 }
