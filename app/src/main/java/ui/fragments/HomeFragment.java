88bb0d594427007bab2748e59a6526be4d98a35d
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-03 04:36:30 +0700

    Apply Data Binding to Fragment Home

--- app/src/main/java/ui/fragments/HomeFragment.java
+++ app/src/main/java/ui/fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package ui.fragments;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,23 +9,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import io.github.reiiyuki.findmything.R;
+import io.github.reiiyuki.findmything.databinding.FragmentHomeBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+    private FragmentHomeBinding binding;
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-
+    public HomeFragment() {}
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false);
+        return binding.getRoot();
     }
 
 }
