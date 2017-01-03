a4d8a42876c49e203494809508a8cc7bb92ff4b7
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-03 15:46:22 +0700

    Initialize Adapter

---
+++ app/src/main/java/adapters/ThingListAdapter.java
@@ -0,0 +1,29 @@
+package adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import models.Thing;
+
+/**
+ * Created by yukir on 1/3/2017.
+ */
+
+public class ThingListAdapter extends RecyclerView.Adapter<Thing>{
+
+    @Override
+    public Thing onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(Thing holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    
+}
