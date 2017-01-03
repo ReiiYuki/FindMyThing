9808af72be0d4934004f318686794e83e4b9110d
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-03 19:51:57 +0700

    Adapter  + ViewHolder

--- app/src/main/java/adapters/ThingListAdapter.java
+++ app/src/main/java/adapters/ThingListAdapter.java
@@ -1,16 +1,21 @@
 package adapters;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import io.github.reiiyuki.findmything.R;
+import io.github.reiiyuki.findmything.databinding.CardThingBinding;
 import io.realm.RealmResults;
 import models.Thing;
+import viewholders.ThingViewHolder;
 
 /**
  * Created by yukir on 1/3/2017.
  */
 
-public class ThingListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+public class ThingListAdapter extends RecyclerView.Adapter<ThingViewHolder>{
 
     RealmResults<Thing> things;
 
@@ -19,17 +24,18 @@ public class ThingListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ThingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardThingBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.card_thing,parent,false);
+        return new ThingViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(ThingViewHolder holder, int position) {
+        holder.getBinding().thingNameText.setText(things.get(position).getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return things.size();
     }
 }
--- app/src/main/java/viewholders/ThingViewHolder.java
+++ app/src/main/java/viewholders/ThingViewHolder.java
@@ -3,12 +3,20 @@ package viewholders;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import io.github.reiiyuki.findmything.databinding.CardThingBinding;
+
 /**
  * Created by yukir on 1/3/2017.
  */
 
 public class ThingViewHolder extends RecyclerView.ViewHolder {
-    public ThingViewHolder(View itemView) {
-        super(itemView);
+    private CardThingBinding binding;
+    public ThingViewHolder(CardThingBinding binding) {
+        super(binding.getRoot());
+        this.binding = binding;
+    }
+
+    public CardThingBinding getBinding() {
+        return binding;
     }
 }
